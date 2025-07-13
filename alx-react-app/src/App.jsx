b0fc06cfe5033568bed23b874c7514a8038f9e83
DuolDaniel <duolkuach100@gmail.com>
2025-07-13 08:26:45 +0300

    components and props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
   return (
@@ -11,6 +13,11 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+        <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
     </div>
   );
 }
