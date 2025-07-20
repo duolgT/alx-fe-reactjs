ba207b1136b283e1084f3efeb1fcfd8c0ad54270
DuolDaniel <duolkuach100@gmail.com>
2025-07-20 09:13:24 +0300

    states and hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <Counter />
         <UserProfile
         name="Alice"
         age="25"
